7b9d938089b18f2108a182152bafc3dcde12bd10
Renzo Crisóstomo <renzo.crisostomomedina@here.com>
2018-09-05 11:47:06 +0200

    Split job into multiple jobs

--- src/jobdsl/Build.groovy
+++ src/jobdsl/Build.groovy
@@ -1,8 +1,11 @@
-job('hello-pe-team') {
-    triggers {
-        scm('*/15 * * * *')
-    }
-    steps {
-        shell('echo "Hello PE team!"')
+def members = ["Renzo", "Stefan", "Yonah", "Dror", "Nati", "Alex", "Yogev"]
+members.each { member ->
+    job("hello-${member}") {
+        triggers {
+            scm('*/15 * * * *')
+        }
+        steps {
+            shell("echo 'Hello ${member}!'")
+        }
     }
 }

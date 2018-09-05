42297722c34f1a8be6cc3ef11b73d138dfcba80a
Renzo Crisóstomo <renzo.crisostomomedina@here.com>
2018-09-05 13:54:41 +0200

    Use configuration

--- src/jobdsl/Build.groovy
+++ src/jobdsl/Build.groovy
@@ -1,11 +1,14 @@
+import com.here.gradle.plugins.jobdsl.util.DslConfig
+
 def members = ["Renzo", "Stefan", "Yonah", "Dror", "Nati", "Alex", "Yogev"]
+def greeting = DslConfig.get('greeting')
 members.each { member ->
     job("hello-${member}") {
         triggers {
             scm('*/15 * * * *')
         }
         steps {
-            shell("echo 'Hello ${member}!'")
+            shell("echo '${greeting} ${member}!'")
         }
     }
 }
